225c717dbbf9da182fbae3bdf64bf095d5b4212a
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2023-11-14 19:00:12 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pxl_20231018_164404559photosphere",
+      "name": "PXL_20231018_164404559.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.09003856049791636,
+        "pitch": 0.10809623549251413,
+        "fov": 1.3683693825392056
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2768844393490788,
+          "pitch": 0.15720576395125008,
+          "rotation": 0,
+          "target": "1-pxl_20231018_164946344photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pxl_20231018_164946344photosphere",
+      "name": "PXL_20231018_164946344.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3853231013077334,
+          "pitch": 0.22938565845645087,
+          "rotation": 0,
+          "target": "0-pxl_20231018_164404559photosphere"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
