2019343fc194a5e487008ec12d472f63d38f0bf2
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2023-11-15 16:59:33 +0300

    Upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-pxl_20231018_164404559photosphere",
-      "name": "PXL_20231018_164404559.PHOTOSPHERE",
+      "id": "0-agc_20231115_134533754photosphere",
+      "name": "AGC_20231115_134533754.PHOTOSPHERE",
       "levels": [
         {
           "tileSize": 256,
@@ -24,23 +24,80 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.09003856049791636,
-        "pitch": 0.10809623549251413,
-        "fov": 1.3683693825392056
+        "yaw": -0.7737586180704952,
+        "pitch": 0.14718528089357008,
+        "fov": 1.3744932219742836
       },
       "linkHotspots": [
         {
-          "yaw": -0.2768844393490788,
-          "pitch": 0.15720576395125008,
+          "yaw": -0.018303696516746015,
+          "pitch": 0.3280362653274942,
           "rotation": 0,
-          "target": "1-pxl_20231018_164946344photosphere"
+          "target": "1-agc_20231115_135212704photosphere"
+        },
+        {
+          "yaw": -1.581863960860538,
+          "pitch": 0.3690348615225503,
+          "rotation": 0,
+          "target": "5-agc_20231115_141214231photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6314932335506285,
+          "pitch": 0.5022104306835402,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-agc_20231115_135212704photosphere",
+      "name": "AGC_20231115_135212704.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7296487478986506,
+        "pitch": 0.3925402318644515,
+        "fov": 1.3744932219742836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6380439952379717,
+          "pitch": 0.3033513828547072,
+          "rotation": 0,
+          "target": "0-agc_20231115_134533754photosphere"
+        },
+        {
+          "yaw": -2.398312178277429,
+          "pitch": 0.2561261687079295,
+          "rotation": 0,
+          "target": "2-agc_20231115_140158462photosphere"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-pxl_20231018_164946344photosphere",
-      "name": "PXL_20231018_164946344.PHOTOSPHERE",
+      "id": "2-agc_20231115_140158462photosphere",
+      "name": "AGC_20231115_140158462.PHOTOSPHERE",
       "levels": [
         {
           "tileSize": 256,
@@ -68,16 +125,155 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.3853231013077334,
-          "pitch": 0.22938565845645087,
+          "yaw": -0.08596697000944253,
+          "pitch": 0.34287204542386185,
           "rotation": 0,
-          "target": "0-pxl_20231018_164404559photosphere"
+          "target": "1-agc_20231115_135212704photosphere"
+        },
+        {
+          "yaw": 1.734533488823521,
+          "pitch": 0.36565272147614003,
+          "rotation": 0,
+          "target": "3-agc_20231115_140532033photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-agc_20231115_140532033photosphere",
+      "name": "AGC_20231115_140532033.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.354140911765036,
+          "pitch": 0.4047650428604683,
+          "rotation": 0,
+          "target": "2-agc_20231115_140158462photosphere"
+        },
+        {
+          "yaw": 0.5654530519196559,
+          "pitch": 0.32531349321105196,
+          "rotation": 0,
+          "target": "4-agc_20231115_140820828photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-agc_20231115_140820828photosphere",
+      "name": "AGC_20231115_140820828.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6078896466463526,
+          "pitch": 0.2969727938112907,
+          "rotation": 0,
+          "target": "3-agc_20231115_140532033photosphere"
+        },
+        {
+          "yaw": 1.482555662240288,
+          "pitch": 0.3292528136860202,
+          "rotation": 0,
+          "target": "5-agc_20231115_141214231photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.778005611025155,
+          "pitch": -0.19046112176940433,
+          "title": "Информационный стенд",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-agc_20231115_141214231photosphere",
+      "name": "AGC_20231115_141214231.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3744932219742836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6319965296055923,
+          "pitch": 0.29081771191952654,
+          "rotation": 0,
+          "target": "4-agc_20231115_140820828photosphere"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Panotour 3D",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
