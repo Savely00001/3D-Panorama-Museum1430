566e3515f6844596b8f2e220ab1ee4a9521eff39
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2023-12-11 16:13:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,13 +47,13 @@
  * } else {
 *    document.body.classList.add('desktop');
 *  }
-*/
-  // Detect whether we are on a touch device.
-  document.body.classList.add('no-touch');
-  window.addEventListener('touchstart', function() {
-    document.body.classList.remove('no-touch');
-    document.body.classList.add('touch');
-  });
+*
+*  // Detect whether we are on a touch device.
+*  document.body.classList.add('no-touch');
+*  window.addEventListener('touchstart', function() {
+  *  document.body.classList.remove('no-touch');
+ *   document.body.classList.add('touch');
+ */ });
 
   // Use tooltip fallback mode on IE < 11.
   if (bowser.msie && parseFloat(bowser.version) < 11) {
