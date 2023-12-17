b6bf1543306f00f43a3ef5074c5f614b5b11435e
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2023-12-17 17:57:01 +0300

    Upload

---
+++ tiles/data.js
@@ -0,0 +1,326 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Вход в музей",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07494099251338682,
+          "pitch": 0.3895234526929787,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": 1.7472569300079286,
+          "pitch": 0.4019506096767742,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -2.3255290482897095,
+          "pitch": 0.3617778599773427,
+          "rotation": 0,
+          "target": "6---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Ближняя часть музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.340070590009109,
+          "pitch": 0.44026359693515005,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 0.5672538874360953,
+          "pitch": 0.41720569738621904,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Средняя часть музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.651447221838005,
+          "pitch": 0.30369762974108205,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": 1.449533698335057,
+          "pitch": 0.39922146581487183,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Конец музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6396929691845727,
+          "pitch": 0.4078674963276647,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Средняя часть музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.411238480454223,
+          "pitch": 0.320090297865363,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 0.6284184920649825,
+          "pitch": 0.36722072909096504,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Конец музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012500817360988847,
+          "pitch": 0.36700550255851105,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.041085325988341,
+          "pitch": 0.683596320236795,
+          "title": "3D модель",
+          "text": "<a href=\"https://sketchfab.com/3d-models/1430-55de61715dec42ec9310dcfb5d836ca4\">1430 - Download Free 3D model by Mr.HRosenberg (@Mr.HRosenberg) [55de617] (sketchfab.com)</a>"
+        },
+        {
+          "yaw": -2.5951997245879035,
+          "pitch": 0.5167301915228677,
+          "title": "3D модель",
+          "text": "<a href=\"https://sketchfab.com/3d-models/1430-55de61715dec42ec9310dcfb5d836ca4\">1430 - Download Free 3D model by Mr.HRosenberg (@Mr.HRosenberg) [55de617] (sketchfab.com)</a>"
+        },
+        {
+          "yaw": -2.192207198646189,
+          "pitch": 0.17318389545301116,
+          "title": "3D модель",
+          "text": "<a href=\"https://sketchfab.com/3d-models/1430-55de61715dec42ec9310dcfb5d836ca4\">1430 - Download Free 3D model by Mr.HRosenberg (@Mr.HRosenberg) [55de617] (sketchfab.com)</a>"
+        }
+      ]
+    },
+    {
+      "id": "6---",
+      "name": "Выход из школы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.00391988784042141,
+          "pitch": -0.11823058288116783,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
