959bb8e086c15657e7e359ac2b10940863a71695
Savely00001 <131238511+Savely00001@users.noreply.github.com>
2024-02-03 12:19:43 +0300

    io2

---
+++ data.js
@@ -0,0 +1,740 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sgcam_20240131_140351953photosphere",
+      "name": "SGCAM_20240131_140351953.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2591235213636907,
+        "pitch": 0.09717123736282396,
+        "fov": 1.3730284701936786
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0064592563325636,
+          "pitch": 0.17511157071663774,
+          "rotation": 0,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Выход из музея",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5206179150331955,
+          "pitch": 0.1895159159022608,
+          "rotation": 0,
+          "target": "0-sgcam_20240131_140351953photosphere"
+        },
+        {
+          "yaw": -0.04755471514093301,
+          "pitch": -0.05643331092145587,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Вход в музей",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0015450038994693,
+          "pitch": 0.5585203774705434,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -0.7589681810427251,
+          "pitch": 0.7615314979709495,
+          "rotation": 0,
+          "target": "3-sgcam_20240131_124345894photosphere"
+        },
+        {
+          "yaw": 0.6232362604526465,
+          "pitch": 0.47036392965403806,
+          "rotation": 0,
+          "target": "14-sgcam_20240131_134255359photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sgcam_20240131_124345894photosphere",
+      "name": "SGCAM_20240131_124345894.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.597165911248581,
+          "pitch": 0.6901798893128586,
+          "rotation": 0,
+          "target": "4-sgcam_20240131_124842314photosphere"
+        },
+        {
+          "yaw": -1.526576344777391,
+          "pitch": 0.9453687288017694,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08812622949751159,
+          "pitch": 0.6226298203470897,
+          "title": "Кирпич",
+          "text": "Результат воздействия лазера на кирпич\nНа кирпиче надпись: «Если кто придет с мечом, с ним станет то, \nчто стало с кирпичом!» …"
+        }
+      ]
+    },
+    {
+      "id": "4-sgcam_20240131_124842314photosphere",
+      "name": "SGCAM_20240131_124842314.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.131602103743985,
+          "pitch": 0.6813672744013619,
+          "rotation": 0,
+          "target": "3-sgcam_20240131_124345894photosphere"
+        },
+        {
+          "yaw": -0.07148034646428769,
+          "pitch": 0.7414940879747043,
+          "rotation": 0,
+          "target": "5-sgcam_20240131_125217970photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sgcam_20240131_125217970photosphere",
+      "name": "SGCAM_20240131_125217970.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2050753283913629,
+          "pitch": 0.7153441310346089,
+          "rotation": 0,
+          "target": "4-sgcam_20240131_124842314photosphere"
+        },
+        {
+          "yaw": 1.9293779757313638,
+          "pitch": 0.7211003443561541,
+          "rotation": 0,
+          "target": "6-sgcam_20240131_125832891photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1496733410593052,
+          "pitch": 0.5974465040685057,
+          "title": "Триумф",
+          "text": "Макет зенитно-ракетного комплекса (ЗРК) С-400 «Триумф»"
+        }
+      ]
+    },
+    {
+      "id": "6-sgcam_20240131_125832891photosphere",
+      "name": "SGCAM_20240131_125832891.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7986454677999397,
+          "pitch": 0.7594774582493535,
+          "rotation": 0,
+          "target": "5-sgcam_20240131_125217970photosphere"
+        },
+        {
+          "yaw": 1.7366247520281988,
+          "pitch": 0.6888865513679683,
+          "rotation": 0,
+          "target": "7-sgcam_20240131_130308949photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sgcam_20240131_130308949photosphere",
+      "name": "SGCAM_20240131_130308949.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0939922967213054,
+          "pitch": 0.7378191198202959,
+          "rotation": 0,
+          "target": "6-sgcam_20240131_125832891photosphere"
+        },
+        {
+          "yaw": 1.5402591864658035,
+          "pitch": 0.7223117298780526,
+          "rotation": 0,
+          "target": "8-sgcam_20240131_131654112photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sgcam_20240131_131654112photosphere",
+      "name": "SGCAM_20240131_131654112.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.641567390965026,
+          "pitch": 0.712057283920263,
+          "rotation": 0,
+          "target": "7-sgcam_20240131_130308949photosphere"
+        },
+        {
+          "yaw": -3.0667668870066507,
+          "pitch": 0.7930276290766578,
+          "rotation": 0,
+          "target": "9-sgcam_20240131_132045231photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6756254731444633,
+          "pitch": 0.7476520221675358,
+          "title": "Обломок",
+          "text": "Обломок баллистической ракеты\nнайденный в степи «специальной группой поиска» после \nочередного испытательного пуска противоракеты"
+        }
+      ]
+    },
+    {
+      "id": "9-sgcam_20240131_132045231photosphere",
+      "name": "SGCAM_20240131_132045231.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.813012162676488,
+          "pitch": 0.741407883140246,
+          "rotation": 0,
+          "target": "8-sgcam_20240131_131654112photosphere"
+        },
+        {
+          "yaw": 0.6921101000681862,
+          "pitch": 0.6208464355047738,
+          "rotation": 0,
+          "target": "10-sgcam_20240131_132700253photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.986816623851727,
+          "pitch": 0.8570175419901798,
+          "title": "Обломок",
+          "text": "Обломок баллистической ракеты\nнайденный в степи «специальной группой поиска» после \nочередного испытательного пуска противоракеты"
+        }
+      ]
+    },
+    {
+      "id": "10-sgcam_20240131_132700253photosphere",
+      "name": "SGCAM_20240131_132700253.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5543705383083406,
+          "pitch": 0.45453456300753103,
+          "rotation": 0,
+          "target": "9-sgcam_20240131_132045231photosphere"
+        },
+        {
+          "yaw": 1.7896344120493257,
+          "pitch": 0.4371516845160208,
+          "rotation": 0,
+          "target": "11-sgcam_20240131_133156557photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08601389975200746,
+          "pitch": 0.9752576501083077,
+          "title": "В-1000",
+          "text": "Макет первой отечественной противоракеты В-1000\nТакой противоракетой 04 марта 1961 года впервые в мире была \nуничтожена головная часть баллистической ракеты"
+        },
+        {
+          "yaw": -0.21948420937073365,
+          "pitch": -0.2793071261122968,
+          "title": "Кисунько",
+          "text": "Кисунько Григорий Васильевич \nГлавный конструктор первой отечественной системы \nпротиворакетной обороны – Системы «А»"
+        }
+      ]
+    },
+    {
+      "id": "11-sgcam_20240131_133156557photosphere",
+      "name": "SGCAM_20240131_133156557.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6947276127671067,
+          "pitch": 0.45953764457316737,
+          "rotation": 0,
+          "target": "10-sgcam_20240131_132700253photosphere"
+        },
+        {
+          "yaw": 1.7525555059464066,
+          "pitch": 0.6839985959388777,
+          "rotation": 0,
+          "target": "12-sgcam_20240131_133526627photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11200201274250254,
+          "pitch": -0.6307448580082742,
+          "title": "Картина Цукова",
+          "text": "Картина Юлия Цукова «Перехват»\nЦуков Ю. К. – ветеран полигона первой волны испытателей, \nполковник, рационализатор и изобретатель, к.т.н., автор многих \nнаучных работ, поэт и художник."
+        }
+      ]
+    },
+    {
+      "id": "12-sgcam_20240131_133526627photosphere",
+      "name": "SGCAM_20240131_133526627.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9839186270024491,
+          "pitch": 0.7519396418654729,
+          "rotation": 0,
+          "target": "13-sgcam_20240131_133939666photosphere"
+        },
+        {
+          "yaw": 1.492815120887065,
+          "pitch": 0.7008180093075129,
+          "rotation": 0,
+          "target": "11-sgcam_20240131_133156557photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5352772631499434,
+          "pitch": 0.4560221934703943,
+          "title": "Ключ",
+          "text": "Символический ключ от Балхашского полигона \nБалхашский полигон, площадью 75 000 кв. км., был основан в \n1956 году в казахской пустыне Бетпак-Дала для проведения \nиспытаний систем ПРО и ПВО"
+        }
+      ]
+    },
+    {
+      "id": "13-sgcam_20240131_133939666photosphere",
+      "name": "SGCAM_20240131_133939666.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9912392930052611,
+          "pitch": 0.684621447547574,
+          "rotation": 0,
+          "target": "12-sgcam_20240131_133526627photosphere"
+        },
+        {
+          "yaw": 1.8746304138218006,
+          "pitch": 0.6472058113786225,
+          "rotation": 0,
+          "target": "14-sgcam_20240131_134255359photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.40004994305773245,
+          "pitch": 0.5604623480800708,
+          "title": "Дон 2Н",
+          "text": "«Дон 2Н»\nМногофункциональная радиолокационная станция (МРЛС), \nстоящая ныне на обороне Москвы и Московского региона"
+        }
+      ]
+    },
+    {
+      "id": "14-sgcam_20240131_134255359photosphere",
+      "name": "SGCAM_20240131_134255359.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7135884292351227,
+          "pitch": 0.4590996003170318,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -2.9987593314003167,
+          "pitch": 0.6892732610787924,
+          "rotation": 0,
+          "target": "13-sgcam_20240131_133939666photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.033803232991905574,
+          "pitch": -0.23783895316216785,
+          "title": "Китель",
+          "text": "Китель Героя Социалистического Труда, генерал-лейтенанта \nГригория Васильевича Кисунько"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
